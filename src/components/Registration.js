c2c4d8c3db1f5b42780d0af606be65fcd140bb44
Tomas Freilij <tomas.freilij@gmail.com>
2020-11-16 18:37:31 -0300

    added react bootstrap to Registration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,87 +1,79 @@
-import React, {useState } from 'react';
-
-import { createUserWithEmailAndPassword, addNewUser } from '../Services/UsersServices'; 
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
-import { useHistory} from 'react-router-dom';
+import { createUserWithEmailAndPassword , addNewUser} from '../Services/UsersServices';
 
 
-const Registration = () => {
-    const history = useHistory();
-    const [form,setForm] = useState({
-      email:'',
-      name:'',
-      surname:'',
-      password:''
-    });
-
-    const handleClick = (e) =>{
-      
-      const name = form.name;
-      const surname = form.surname;
-      const email = form.email;
-      const password = form.password;
-
-      createUserWithEmailAndPassword(email,password)
-      .then(data => {
-        const userId = data.user.uid;
-      
-        addNewUser(name,surname,email,userId)
-        .then( data => {
-               console.log("user added ",data );
-               history.push("/")
-        })
-        .catch( error => {
-              console.log("user error", error);
-        })
-      })
-      .catch( err => {
-          console.log("there was an error in the user registration", err);
-      })
-
-
-
-      
-      e.preventDefault();
-
-
-    }
-
-    const handleChange = (e) => {
-      const target = e.target;
-
-      setForm({
-        ...form,
-        [target.name]:target.value
-      })
-    }
-
+function Registration() {
    
-    return(
-      <Form>
-      <Form.Group controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control onChange={handleChange} type="email" name="email" value={form.email} placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control onChange={handleChange} type="password" name="password" value={form.password} placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formGroupName">
+    const [form,setForm] = useState({name:'',
+                                    apellido:'',
+                                    email:'',
+                                    password:''
+                                    });
+    
+    const handleSubmit = (e) => {
+
+        const email = form.email;
+        const password = form.password;
+        const name  = form.name;
+        const surname = form.surname
+
+        createUserWithEmailAndPassword(email,password)
+        .then( data => {
+            const userId = data.user.uid
+            console.log(data)
+            addNewUser(name,surname,email,userId)
+            .then(data => { console.log("data database",data)})
+            .catch(error => {
+                console.log("error database", error)  
+            })
+            }
+        )
+        .catch(err => {
+            console.log(err)
+        })
+
+    }
+    const handleChange = (e) => {
+        const target = e.target;
+        const name = target.name;
+        const value = target.value;
+
+        setForm({
+            ...form,
+            [name]:value
+        });
+    }
+
+    return (
+
+        <Form>
+            <Form.Group >
         <Form.Label>Name</Form.Label>
-        <Form.Control onChange={handleChange} type="text" name="name" value ={form.name} placeholder="Name" />
+        <Form.Control onChange={handleChange} type="text" name="name" placeholder="Password" />
       </Form.Group>
-      <Form.Group controlId="formGroupSurname">
+      <Form.Group >
         <Form.Label>Surname</Form.Label>
-        <Form.Control onChange={handleChange} type="text" name="surname" value={form.surname} placeholder="Surname" />
+        <Form.Control onChange={handleChange} type="text" name="surname" placeholder="Password" />
       </Form.Group>
-      <Button onClick={handleClick} variant="primary" type="submit" >
-        Accept
+      <Form.Group >
+        <Form.Label>Email address</Form.Label>
+        <Form.Control onChange={handleChange} type="email" name="email" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group >
+        <Form.Label>Password</Form.Label>
+        <Form.Control onChange={handleChange} type="password" name="password" placeholder="Password" />
+      </Form.Group>
+
+      <Button onClick={handleSubmit} variant="primary" type="submit">
+       Accept
       </Button>
       
     </Form>
-    ) 
+      
+    )
 
-       
+    
 
 }
 
